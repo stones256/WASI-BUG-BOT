4fc99a0b8d07a4232a83f3cc198ebf9901c4394e
stones256 <stonebankz07@gmail.com>
2024-09-24 14:08:48 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "256707857899"
+global.ownername = "Aitham Banks "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
